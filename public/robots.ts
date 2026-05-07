91cad567f73d9ad4c1d29673b9aa96c17d1689f4
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-05-07 21:15:14 +0600

    update roboto file

--- public/robots.ts
+++ public/robots.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://mdmahfuzulhaque3140.vercel.app';
-  
+
   return {
     rules: {
       userAgent: '*',
